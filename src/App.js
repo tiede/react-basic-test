bc9fb672b08f79c39d39bb834016eb0944a9156f
Kim Tiedemann <kim@tiede.dk>
2023-02-09 22:56:55 +0100

    Service call works

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {useEffect, useState} from 'react';
 
 function App() {
-   const [weather, setWeather] = useState([]);
+   const [weather, setWeather] = useState({});
    const url = 'https://api.weatherapi.com/v1/current.json?key=3de6c9f8c2504acaa36202353230902&q=Copenhagen&aqi=no';
    useEffect(() => {
       fetch(url)
          .then((response) => response.json())
          .then((data) => {
             console.log(data);
-            setWeather(data);
+            setWeather(data.current);
          })
          .catch((err) => {
             console.log(err.message);
@@ -17,19 +17,13 @@ function App() {
    }, []);
    
    const value = 'Kim';
-   return 
-      (
+   return (
          <div>
             <div>Hello {value}</div>
-            <div>
-               {weather.location}
-            </div>
+            <div>Temp { weather.temp_c } </div>
+            <div>Temp (feels like) { weather.feelslike_c } </div>
          </div>
       );
-}
-
-function GetWeather() {
-  
 }
 
 export default App;
