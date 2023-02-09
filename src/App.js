8fb206a21d475470a9232b16e6f5efeb6aa73ef8
Kim Tiedemann <kim@tiede.dk>
2023-02-09 22:14:46 +0100

    Rendering weather data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-//import {useEffect, useState} from 'react';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 function App() {
-  GetWeather();
-  const value = 'Kim';
-  return <div>Hello {value}</div>;
+   const [weather, setWeather] = useState([]);
+   const url = 'https://api.weatherapi.com/v1/current.json?key=3de6c9f8c2504acaa36202353230902&q=Copenhagen&aqi=no';
+   useEffect(() => {
+      fetch(url)
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            setWeather(data);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   }, []);
+   
+   const value = 'Kim';
+   return 
+      (
+         <div>
+            <div>Hello {value}</div>
+            <div>
+               {weather.location}
+            </div>
+         </div>
+      );
 }
 
 function GetWeather() {
-  const url = 'https://api.weatherapi.com/v1/current.json?key=3de6c9f8c2504acaa36202353230902&q=Copenhagen&aqi=no';
-  useEffect(() => {
-    fetch(url)
-       .then((response) => response.json())
-       .then((data) => {
-          console.log(data);
-          //setPosts(data);
-       })
-       .catch((err) => {
-          console.log(err.message);
-       });
- }, []);
+  
 }
 
 export default App;
